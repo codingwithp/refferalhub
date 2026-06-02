e5fc8732c4a4b359690b199790a2ecd438a0cd28
Pragathi <pragathi.is22@bmsce.ac.in>
2026-06-02 11:25:23 +0530

    tryna crt it

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -4,36 +4,38 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  family: 4, // Force IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
 
-exports.sendVoucherEmail =
-  async (email, name) => {
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: email,
-      subject:
-        "Referral Reward Earned",
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("SMTP VERIFY ERROR:", error);
+  } else {
+    console.log("SMTP READY");
+  }
+});
 
-      html: `
-        <h2>Hello ${name}</h2>
+exports.sendVoucherEmail = async (email, name) => {
+  return await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: "Referral Reward Earned",
+    html: `
+      <h2>Hello ${name}</h2>
 
-        <p>
-          Congratulations!
-        </p>
+      <p>Congratulations!</p>
 
-        <p>
-          One of your referrals has
-          successfully converted.
-        </p>
+      <p>
+        One of your referrals has successfully converted.
+      </p>
 
-        <p>
-          You are eligible for an
-          Amazon Gift Voucher.
-        </p>
-      `,
-    });
-  };
+      <p>
+        You are eligible for an Amazon Gift Voucher.
+      </p>
+    `,
+  });
+};
