043e9d803cc4f5ca6e04f19f65b461734cdbf75a
Pragathi <pragathi.is22@bmsce.ac.in>
2026-06-02 11:44:33 +0530

    correction

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,13 +1,15 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "74.125.24.108", // smtp.gmail.com IPv4
   port: 587,
   secure: false,
-  family: 4, // Force IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
