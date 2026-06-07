2283b38f12ed0ba177812273228bef072a6fa803
Pragathi <pragathi.is22@bmsce.ac.in>
2026-06-07 19:03:29 +0530

    please hojaa yaar

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,13 +1,17 @@
+
+require("dns").setDefaultResultOrder("ipv4first");
 const nodemailer = require("nodemailer");
 
-const transporter =
-  nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 exports.sendVoucherEmail = async (
   email,
